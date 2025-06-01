4a1b86b2220c7572aad02f79fd802261ddf2f318
ChickenRuner <forfunforstream@gmail.com>
2025-06-02 01:21:31 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -47,8 +47,8 @@ loader.load('/models/chicken.glb', (gltf) => {
 // Загрузка машины
 loader.load('/models/car1.glb', (gltf) => {
   car1 = gltf.scene;
-  car1.scale.set(3, 3, 3); // увеличим в 3 раза
-  car1.position.set(1.5, -0.85, -30); // немного приподнимем
+  car1.scale.set(10, 10, 10); // сильно увеличим
+  car1.position.set(1.5, -0.75, -30); // поднимаем чуть выше уровня дороги
   scene.add(car1);
 }, undefined, (error) => {
   console.error('Ошибка загрузки машины:', error);
